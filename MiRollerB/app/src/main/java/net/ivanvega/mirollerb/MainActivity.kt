c029bc17e28522e667d6b876dca3f22369041ac7
Gustavo Ivan Vega Olvera <isc.givox@gmail.com>
2022-09-01 11:52:29 -0500

    Se agregaron las imagenes para la app roller

--- MiRollerB/app/src/main/java/net/ivanvega/mirollerb/MainActivity.kt
+++ MiRollerB/app/src/main/java/net/ivanvega/mirollerb/MainActivity.kt
@@ -4,15 +4,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    lateinit var etiqueta: TextView
+    lateinit var boton : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.layout_roller)
 
-        val boton : Button = findViewById(R.id.btnClick)
-
+        boton = findViewById(R.id.btnClick)
+        etiqueta = findViewById(R.id.lblRoller)
         //forma larga de asignar listener
         /*
         boton.setOnClickListener(View.OnClickListener {
@@ -23,8 +27,15 @@ class MainActivity : AppCompatActivity() {
         boton.setOnClickListener {
             Toast.makeText(applicationContext,
                 "Boton presionado", Toast.LENGTH_LONG ).show()
+
+            val aleatorio = diceRoller()
+            etiqueta.text = "Alatorio ${aleatorio}"
+
         }
-
-
     }
+
+    fun diceRoller(par: Int = 0): Int{
+        return (1..6).random()
+    }
+
 }
