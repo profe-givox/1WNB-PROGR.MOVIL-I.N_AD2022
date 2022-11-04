03e0a5c9135418b658781453b268196b5c2732bf
el givo <gi.vega@itsur.edu.mx>
2022-11-04 11:51:14 -0600

    El proyecto ya toma fotos, las administra a través  del sistema de archivos, las carga en un imageview y tambien las puede escalar

--- MisNotasB/app/src/main/java/net/ivanvega/misnotasb/MultimediaActivity.kt
+++ MisNotasB/app/src/main/java/net/ivanvega/misnotasb/MultimediaActivity.kt
@@ -2,12 +2,21 @@ package net.ivanvega.misnotasb
 
 import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
+import androidx.core.content.FileProvider
 import net.ivanvega.misnotasb.databinding.ActivityMultimediaBinding
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MultimediaActivity : AppCompatActivity() {
+    lateinit var photoURI: Uri
     lateinit var binding :ActivityMultimediaBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,23 +29,89 @@ class MultimediaActivity : AppCompatActivity() {
         }
     }
 
-    val REQUEST_IMAGE_CAPTURE = 1
+    val REQUEST_TAKE_PHOTO = 1
 
     private fun dispatchTakePictureIntent() {
         Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            // Ensure that there's a camera activity to handle the intent
             takePictureIntent.resolveActivity(packageManager)?.also {
+                // Create the File where the photo should go
+                val photoFile: File? = try {
+                    createImageFile()
+                } catch (ex: IOException) {
+                    // Error occurred while creating the File
 
-                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+                    null
+                }
+                // Continue only if the File was successfully created
+                photoFile?.also {
+                     photoURI= FileProvider.getUriForFile(
+                        this,
+                        "net.ivanvega.misnotasb.fileprovider",
+                        it
+                    )
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                    startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO)
+                }
             }
         }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            val imageBitmap = data?.extras?.get("data") as Bitmap
-            binding.imageView.setImageBitmap(imageBitmap)
+        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
+//            val imageBitmap = data?.extras?.get("data") as Bitmap
+//            binding.imageView.setImageBitmap(imageBitmap)
+
+            //cargo la imagen dada una uri
+            //binding.imageView.setImageURI(photoURI)
+
+            //CARGO UNA IMAGEN ESCALADA
+            setPic()
         }
     }
+
+    lateinit var currentPhotoPath: String
+
+    @Throws(IOException::class)
+     fun createImageFile(): File {
+        // Create an image file name
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val storageDir: File? = getExternalFilesDir(null)
+        return File.createTempFile(
+            "JPEG_${timeStamp}_", /* prefix */
+            ".jpg", /* suffix */
+            storageDir /* directory */
+        ).apply {
+            // Save a file: path for use with ACTION_VIEW intents
+            currentPhotoPath = absolutePath
+        }
+    }
+
+    private fun setPic() {
+        // Get the dimensions of the View
+        val targetW: Int = binding.imageView.width
+        val targetH: Int = binding.imageView.height
+
+        val bmOptions = BitmapFactory.Options().apply {
+            // Get the dimensions of the bitmap
+            inJustDecodeBounds = true
+
+            val photoW: Int = outWidth
+            val photoH: Int = outHeight
+
+            // Determine how much to scale down the image
+            val scaleFactor: Int = Math.min(photoW / targetW, photoH / targetH)
+
+            // Decode the image file into a Bitmap sized to fill the View
+            inJustDecodeBounds = false
+            inSampleSize = scaleFactor
+            inPurgeable = true
+        }
+        BitmapFactory.decodeFile(currentPhotoPath, bmOptions)?.also { bitmap ->
+            binding.imageView.setImageBitmap(bitmap)
+        }
+    }
+
 
 }
